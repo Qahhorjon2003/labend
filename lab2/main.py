636d137e15030820bac677af4ea016e34e0f7008
Qahhorjon2003 <sidikov22022003@gmail.com>
2022-11-01 11:18:07 +0300

    commit

---
+++ lab2/main.py
@@ -0,0 +1,28 @@
+a = int(input())
+b = int(input())
+if a < b:
+    print(a)
+else:
+    print(b)
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b:
+    print(a)
+else:
+    print(b)
+if b > c:
+    print(b)
+else:
+    print(c)
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+if num1 == num2 and num1 == num3 and num3 == num2:
+    print(0)
+elif num1 == num2 or num1 == num3 or num3 == num2:
+    print(1)
+elif num1 != num2 and num2!=num3 and num1!=num3:
+    print(3)
