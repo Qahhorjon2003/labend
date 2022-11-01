86ca70fe85b6bd3e2e80956d3eb0e0fb0fac6bd2
Qahhorjon2003 <sidikov22022003@gmail.com>
2022-11-01 11:52:52 +0300

    commit

--- lab2/main.py
+++ lab2/main.py
@@ -26,5 +26,5 @@ if num1 == num2 and num1 == num3 and num3 == num2:
     print(0)
 elif num1 == num2 or num1 == num3 or num3 == num2:
     print(1)
-elif num1 != num2 and num2!=num3 and num1!=num3:
+elif num1 != num2 and num2 !=num3 and num1 !=num3:
     print(3)
