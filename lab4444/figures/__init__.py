b298461bc87e489887e22996a5f80c61293c8938
Qahhorjon2003 <116804078+Qahhorjon2003@users.noreply.github.com>
2022-12-27 19:27:43 +0300

    lab4444

--- lab4444/figures/__init__.py
+++ lab4444/figures/__init__.py
@@ -1,3 +1,4 @@
-from .circle import *
-from .triangle import *
-from .square import *
+from figures import *
+if __name__ == "__main__":
+    radius = float(input("enter the radius: "))
+    circle_area(radius)
