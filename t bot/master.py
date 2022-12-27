0851c7c4f4de4ff49708520fb7a2e44e9fbdfadc
Qahhorjon2003 <116804078+Qahhorjon2003@users.noreply.github.com>
2022-12-27 15:37:45 +0300

    123

---
+++ t bot/master.py
@@ -0,0 +1,29 @@
+import telebot
+import 
+
+bot = telebot.TeleBot('5922157685:AAH1YBquQjxMroPzXhij-G3gkhmVZ8r708o')
+
+
+@bot.message_handler(commands=['start'])
+def start (message):
+ bot.send_message(message.chat.id,'Приветики!!!<3 Я могу отправить тебе фото котиков, если ты этого захочешь. Напиши /help, чтобы узнать мои команды:)')
+
+@bot.message_handler(commands=['help'])
+def help(:
+ bot.send_message(message.chat.id,'Чтобы получить фото, напиши /maw')
+
+@bot.message_handler(commands=['maw'])
+def meow(message):
+    r = requests.get('http://thecatapi.com/api/images/get?format=src')
+    url = r.url
+    bot.send_photo(message.chat.id , url)
+
+
+
+@bot.message_handler()
+def get_user_text (message):
+    if message.text == "Привет":
+        bot.send_message(message.chat.id,"Приветики, напиши /help")
+    else:
+        bot.send_message(message.chat.id,"Напиши /help, чтобы узнать мои команды!!!")
+        
